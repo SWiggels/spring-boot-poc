e1d3364c8273daf5290ee31ddcf22ed7d295fbc7
reasonthearchitect <rhys.personal@gmail.com>
2014-09-05 05:53:56 -0400

    Added the in memory database configuration with H2

--- java/src/main/groovy/com/tasc/poc/Application.groovy
+++ java/src/main/groovy/com/tasc/poc/Application.groovy
@@ -1,14 +1,28 @@
 package com.tasc.poc;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
+	
+	@Bean
+	public DataSource dataSource() {
+	  DataSource bean = new EmbeddedDatabaseBuilder()
+		.setType(EmbeddedDatabaseType.H2)
+		.addScript("classpath:schema.sql")
+		.build();
+	  return bean;
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
