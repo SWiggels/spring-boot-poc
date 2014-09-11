cc53385f5fd1eb950ab3b26263c8cc1182335f8c
agruszynski <agruszynski@gmail.com>
2014-09-10 22:32:24 -0500

    More poc

--- angular-java-poc/angular-server/app.js
+++ angular-java-poc/angular-server/app.js
@@ -1,34 +1,29 @@
-// angular.module('golden', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'clientModule', 'participantModule', 'myModule']);
-
-// angular.module('golden').config(function($stateProvider, $urlRouterProvider) {
-//     $urlRouterProvider.otherwise('/main');
-// });
-
-// angular.module('golden').run(function($rootScope) {
-
-//     $rootScope.safeApply = function(fn) {
-//         var phase = $rootScope.$$phase;
-//         if (phase === '$apply' || phase === '$digest') {
-//             if (fn && (typeof(fn) === 'function')) {
-//                  fn();
-//             }
-//         } else {
-//             this.$apply(fn);
-//         } 
-//     };
-    
-// });
-
-var golden = angular.module('golden', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'clientModule', 'participantModule', 'myModule']);
-
+angular.module('golden', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'clientModule', 'participantModule', 'myModule']);
 
 angular.element(document).ready(function() {
+    // once golden loads mods, then config
     angular.module('myModule').config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/main');
     });
     angular.module('clientModule').config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/clients');
     });
+    // manual bootstrap to the page
     angular.bootstrap(document.getElementById("app1"), ['myModule']);
     angular.bootstrap(document.getElementById("app2"), ['clientModule']);
+});
+
+angular.module('golden').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                 fn();
+            }
+        } else {
+            this.$apply(fn);
+        } 
+    };
+    
 });
