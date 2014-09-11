f61cae56ed7406a2305f97f2449108f54e591e05
Adam Gruszynski <agruszynski@gmail.com>
2014-09-10 21:18:27 -0500

    test commit

---
+++ angular-java-poc/angular-server/Gruntfile.js
@@ -0,0 +1,281 @@
+/*jslint node: true */
+'use strict';
+
+var pkg = require('./package.json');
+
+//Using exclusion patterns slows down Grunt significantly
+//instead of creating a set of patterns like '**/*.js' and '!**/node_modules/**'
+//this method is used to create a set of inclusive patterns for all subdirectories
+//skipping node_modules, bower_components, dist, and any .dirs
+//This enables users to create any directory structure they desire.
+var createFolderGlobs = function(fileTypePatterns) {
+  fileTypePatterns = Array.isArray(fileTypePatterns) ? fileTypePatterns : [fileTypePatterns];
+  var ignore = ['node_modules','bower_components','dist','temp'];
+  var fs = require('fs');
+  return fs.readdirSync(process.cwd())
+          .map(function(file){
+            if (ignore.indexOf(file) !== -1 ||
+                file.indexOf('.') === 0 ||
+                !fs.lstatSync(file).isDirectory()) {
+              return null;
+            } else {
+              return fileTypePatterns.map(function(pattern) {
+                return file + '/**/' + pattern;
+              });
+            }
+          })
+          .filter(function(patterns){
+            return patterns;
+          })
+          .concat(fileTypePatterns);
+};
+
+module.exports = function (grunt) {
+
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+  
+	express: {
+		options: {
+			  // Override the command used to start the server.
+			// (do not use 'coffee' here, the server will not be able to restart
+			//  see below at opts for coffee-script support)
+			cmd: process.argv[0],
+
+			// Will turn into: `node OPT1 OPT2 ... OPTN path/to/server.js ARG1 ARG2 ... ARGN`
+			// (e.g. opts: ['node_modules/coffee-script/bin/coffee'] will correctly parse coffee-script)
+			opts: [ ],
+			args: [ ],
+
+			// Setting to `false` will effectively just run `node path/to/server.js`
+			background: true,
+
+			// Called when the spawned server throws errors
+			fallback: function() {},
+
+			// Override node env's PORT
+			port: 9001,
+
+			// Override node env's NODE_ENV
+			node_env: undefined,
+
+			// Consider the server to be "running" after an explicit delay (in milliseconds)
+			// (e.g. when server has no initial output)
+			delay: 0,
+
+			// Regular expression that matches server output to indicate it is "running"
+			output: ".+",
+
+			// Set --debug
+			debug: false
+		},
+		dev: {
+			options: {
+				script: 'proxy-server.js'
+			}
+		}
+	},
+    connect: {
+      main: {
+        options: {
+          port: 9001
+        }
+      }
+    },
+    watch: {
+      main: {
+        options: {
+            livereload: true,
+            livereloadOnError: false,
+            spawn: false
+        },
+        files: [createFolderGlobs(['*.js','*.less','*.html']),'!_SpecRunner.html','!.grunt'],
+        tasks: [] //all the tasks are run dynamically during the watch event handler
+      }
+    },
+    jshint: {
+      main: {
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        src: createFolderGlobs('*.js')
+      }
+    },
+    clean: {
+      before:{
+        src:['dist','temp']
+      },
+      after: {
+        src:['temp']
+      }
+    },
+    less: {
+      production: {
+        options: {
+        },
+        files: {
+          'temp/app.css': 'app.less'
+        }
+      }
+    },
+    ngtemplates: {
+      main: {
+        options: {
+            module: pkg.name,
+            htmlmin:'<%= htmlmin.main.options %>'
+        },
+        src: [createFolderGlobs('*.html'),'!index.html','!_SpecRunner.html'],
+        dest: 'temp/templates.js'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['img/**'], dest: 'dist/'},
+          {src: ['bower_components/font-awesome/fonts/**'], dest: 'dist/',filter:'isFile',expand:true}
+          //{src: ['bower_components/angular-ui-utils/ui-utils-ieshiv.min.js'], dest: 'dist/'},
+          //{src: ['bower_components/select2/*.png','bower_components/select2/*.gif'], dest:'dist/css/',flatten:true,expand:true},
+          //{src: ['bower_components/angular-mocks/angular-mocks.js'], dest: 'dist/'}
+        ]
+      }
+    },
+    dom_munger:{
+      read: {
+        options: {
+          read:[
+            {selector:'script[data-concat!="false"]',attribute:'src',writeto:'appjs'},
+            {selector:'link[rel="stylesheet"][data-concat!="false"]',attribute:'href',writeto:'appcss'}
+          ]
+        },
+        src: 'index.html'
+      },
+      update: {
+        options: {
+          remove: ['script[data-remove!="false"]','link[data-remove!="false"]'],
+          append: [
+            {selector:'body',html:'<script src="app.full.min.js"></script>'},
+            {selector:'head',html:'<link rel="stylesheet" href="app.full.min.css">'}
+          ]
+        },
+        src:'index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      main: {
+        src:['temp/app.css','<%= dom_munger.data.appcss %>'],
+        dest:'dist/app.full.min.css'
+      }
+    },
+    concat: {
+      main: {
+        src: ['<%= dom_munger.data.appjs %>','<%= ngtemplates.main.dest %>'],
+        dest: 'temp/app.full.js'
+      }
+    },
+    ngmin: {
+      main: {
+        src:'temp/app.full.js',
+        dest: 'temp/app.full.js'
+      }
+    },
+    uglify: {
+      main: {
+        src: 'temp/app.full.js',
+        dest:'dist/app.full.min.js'
+      }
+    },
+    htmlmin: {
+      main: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true
+        },
+        files: {
+          'dist/index.html': 'dist/index.html'
+        }
+      }
+    },
+    imagemin: {
+      main:{
+        files: [{
+          expand: true, cwd:'dist/',
+          src:['**/{*.png,*.jpg}'],
+          dest: 'dist/'
+        }]
+      }
+    },
+    karma: {
+      options: {
+        frameworks: ['jasmine'],
+        files: [  //this files data is also updated in the watch handler, if updated change there too
+          '<%= dom_munger.data.appjs %>',
+          'bower_components/angular-mocks/angular-mocks.js',
+          createFolderGlobs('*-spec.js')
+        ],
+        logLevel:'ERROR',
+        reporters:['mocha'],
+        autoWatch: false, //watching is handled by grunt-contrib-watch
+        singleRun: true
+      },
+      all_tests: {
+        browsers: ['PhantomJS','Chrome','Firefox']
+      },
+      during_watch: {
+        browsers: ['PhantomJS']
+      },
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-express-server');
+
+  grunt.registerTask('build',['jshint','clean:before','less','dom_munger','ngtemplates','cssmin','concat','ngmin','uglify','copy','htmlmin','imagemin','clean:after']);
+  grunt.registerTask('serve', ['dom_munger:read','jshint','connect', 'watch']);
+  grunt.registerTask('test',['dom_munger:read','karma:all_tests']);
+  grunt.registerTask('run', [ 'dom_munger:read','jshint','express:dev',  'watch' ]);
+  
+  grunt.event.on('watch', function(action, filepath) {
+    //https://github.com/gruntjs/grunt-contrib-watch/issues/156
+
+    var tasksToRun = [];
+
+    if (filepath.lastIndexOf('.js') !== -1 && filepath.lastIndexOf('.js') === filepath.length - 3) {
+
+      //lint the changed js file
+      grunt.config('jshint.main.src', filepath);
+      tasksToRun.push('jshint');
+
+      //find the appropriate unit test for the changed file
+      var spec = filepath;
+      if (filepath.lastIndexOf('-spec.js') === -1 || filepath.lastIndexOf('-spec.js') !== filepath.length - 8) {
+        spec = filepath.substring(0,filepath.length - 3) + '-spec.js';
+      }
+
+      //if the spec exists then lets run it
+      if (grunt.file.exists(spec)) {
+        var files = [].concat(grunt.config('dom_munger.data.appjs'));
+        files.push('bower_components/angular-mocks/angular-mocks.js');
+        files.push(spec);
+        grunt.config('karma.options.files', files);
+        tasksToRun.push('karma:during_watch');
+      }
+    }
+
+    //if index.html changed, we need to reread the <script> tags so our next run of karma
+    //will have the correct environment
+    if (filepath === 'index.html') {
+      tasksToRun.push('dom_munger:read');
+    }
+
+    grunt.config('watch.main.tasks',tasksToRun);
+
+  });
+};
---
+++ angular-java-poc/angular-server/README.md
@@ -0,0 +1,77 @@
+# Web Project
+
+> Template set up in order to develop angularjs applications using node as a proxy server.
+
+## A Few Notes
+
+Note that this version of the app uses  AngularUI Router [ui-router](https://github.com/angular-ui/ui-router), as a result it is recommended that you use the cli for adding purpose to the app.
+
+Also note that [http-proxy](https://github.com/nodejitsu/node-http-proxy) has been installed in this package.
+
+## Modules????
+
+This doc does not intent to go over all the reasons for modules, the preceding link does a good job of that. As you know, modules are coming in ES6.
+
+Angular modules are covered [here](https://docs.angularjs.org/guide/module)
+
+Finally, as a high level understanding of modules the following is provided.
+```Shell
+A module for each feature
+A module for each reusable component (especially directives and filters)
+And an application level module which depends on the above modules and contains any initialization code.
+```
+
+## More Reading
+
+[Begin here](https://medium.com/@dickeyxxx/best-practices-for-building-angular-js-apps-266c1a4a6917)
+
+[Next]https://medium.com/opinionated-angularjs/scalable-code-organization-in-angularjs-9f01b594bf06
+
+[Then](https://medium.com/opinionated-angularjs/a2fcbf874a1c)
+
+[ng-boilerplate](https://github.com/ngbp/ngbp#readme)
+
+[Font Awesome](http://fortawesome.github.io/Font-Awesome/)
+
+[Placeholders](http://joshdmiller.github.io/angular-placeholders/)
+
+[Google best practices](https://docs.google.com/document/d/1XXMvReO8-Awi1EZXAXS4PzDzdNvV6pGcuaF4Q9821Es/pub)
+
+[Google style guide](https://google-styleguide.googlecode.com/svn/trunk/angularjs-google-style.html)
+
+[angular-app]{https://github.com/angular-app/angular-app/}
+
+Read the [book](https://www.packtpub.com/web-development/mastering-web-application-development-angularjs)
+
+## Finally... Install Base Requirements 
+
+If you have not already, install [Git](http://http://git-scm.com/downloads) on your machine. You will need to use the Git Bash shell for running the server.
+
+Additionally, you will need to install [node.js/npm](http://nodejs.org/) to run this example.
+
+## Project Specific Intall Requirements
+
+Add [Yeoman and Bower](http://yeoman.io/)
+
+```shell
+npm install -g grunt-cli yo bower
+```
+
+Add the [enterprise package development generator](https://github.com/cgross/generator-cg-angular) for angular.
+```shell
+npm install -g generator-cg-angular
+```
+
+## Install Dependancies
+
+You will need to now install the projects dependencies.
+```shell
+bower install & npm install
+```
+
+## Run The Server
+
+In order to run the server, as grunt has been extended to support the proxy pass through
+```shell
+grunt run
+```
