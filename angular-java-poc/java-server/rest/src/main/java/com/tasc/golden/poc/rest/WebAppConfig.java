164bc1fd0f75d2f53bc0d8a039c3751edc825d93
Jason Sicotte <sicotte.jason@gmail.com>
2014-09-29 07:49:00 -0500

    Initial commit of swagger support

---
+++ angular-java-poc/java-server/rest/src/main/java/com/tasc/golden/poc/rest/WebAppConfig.java
@@ -0,0 +1,11 @@
+package com.tasc.golden.poc.rest;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.mangofactory.swagger.plugin.EnableSwagger;
+
+@Configuration
+@EnableSwagger
+public class WebAppConfig {
+
+}
